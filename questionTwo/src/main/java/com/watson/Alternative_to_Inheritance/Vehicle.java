f3b11ea70e0c6f159b3452f8bdd49b3e3bdb1ddd
Watson9023 <matunhirewendy@gmail.com>
2018-03-10 12:29:14 +0200

    Added questionTwo - Alternative to inheritance - complete

---
+++ questionTwo/src/main/java/com/watson/Alternative_to_Inheritance/Vehicle.java
@@ -0,0 +1,45 @@
+package com.watson.Alternative_to_Inheritance;
+
+public class Vehicle {
+
+    private int numOfSeats;
+    private int numOfWheels;
+    private String vehType;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(int numOfSeats, int numOfWheels, String vehType) {
+        this.numOfSeats = numOfSeats;
+        this.numOfWheels = numOfWheels;
+        this.vehType = vehType;
+    }
+
+    public int getNumberOfSeats() {
+        return numOfSeats;
+    }
+
+    public int getNumberOfWheels() {
+        return numOfWheels;
+    }
+
+    public String getVehicleType() {
+        return vehType;
+    }
+    public void move(){
+        System.out.print("Vehicles are used for moving from one place to another.");
+    }
+
+    public void drive(){
+        System.out.print("You are driving a vehicle.");
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "numOfSeats=" + numOfSeats +
+                ", numOfWheels=" + numOfWheels +
+                ", vehType='" + vehType + '\'' +
+                '}';
+    }
+}
---
+++ questionTwo/src/main/java/com/watson/Before.java
@@ -0,0 +1,4 @@
+package com.watson;
+
+public @interface Before {
+}
